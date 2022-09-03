4e85a80662bb2eb26614d54e88447c92c64a1d09
Nguyễn Thanh Nam <nguyenthanhnam1004@gmail.com>
2022-09-03 17:32:53 +0700

    cm

--- src/server.js
+++ src/server.js
@@ -8,11 +8,30 @@ import connectDB from './config/connectDB'
 require("dotenv").config();
 
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-}));
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,            //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+// }));
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.URL_ORIGIN);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
